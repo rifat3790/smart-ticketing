85e4b5eea1f2963826b527f9477d4be5a15d721e
rifat3790 <mdrifayethossen@gmail.com>
2024-08-20 23:11:59 +0600

    added js for seat selected and seat cound

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,53 @@
-function buyTicket(){
-    console.log('Buying ticket...');
-    document.getElementById('ph-paribahan').scrollIntoView({ behavior: 'smooth' })
+function buyTicket() {
+  console.log("Buying ticket...");
+  document
+    .getElementById("ph-paribahan")
+    .scrollIntoView({ behavior: "smooth" });
 }
+
+document.getElementById("A1").addEventListener("click", function () {
+  console.log("A1 clicked");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const seats = document.querySelectorAll(".serial-number");
+
+  seats.forEach((seat) => {
+    seat.addEventListener("click", function () {
+      // check if seat is already booked
+      // console.log(seat.classList.contains('booked'));
+      if (!seat.classList.contains("booked")) {
+        seat.classList.add("booked");
+        seat.classList.add("bg-[#1DD100]");
+        seat.classList.add("text-white");
+        removeElementClass("booked-seats", "hidden");
+
+        // get the seat number
+        const seatText = seat.innerText;
+        console.log(`Seat ${seatText} selected`);
+
+        const selectSeat = getElementByIdText("selected-seat");
+
+        const div = document.createElement("div");
+        div.className = "font-inter flex justify-between text-[#03071299]";
+        div.innerHTML = `
+                    <p>${seatText}</p>
+                    <p>Economy</p>
+                    <p>500</p>`;
+                    selectSeat.appendChild(div);
+
+        // seat number decrease
+        let avaliableSeat = getElementByIdIntegerValue("avaliable-seat");
+        avaliableSeat--;
+        setElementByIdIntegerValue("avaliable-seat", avaliableSeat);
+        
+        // seat count increase
+        let seatCount = getElementByIdIntegerValue("seat-count");
+        seatCount++;
+        setElementByIdIntegerValue("seat-count", seatCount);
+      } else {
+        console.log("Seat is already booked");
+      }
+    });
+  });
+});
---
+++ scripts/utility.js
@@ -0,0 +1,19 @@
+function removeElementClass(elementId, className){
+    document.getElementById(elementId).classList.remove(className);
+}
+
+function addElementClass(elementId, className){
+    document.getElementById(elementId).classList.add(className);
+}
+
+function getElementByIdIntegerValue(elementId){
+    return parseInt(document.getElementById(elementId).textContent);
+}
+
+function getElementByIdText(elementId){
+    return document.getElementById(elementId);
+}
+
+function setElementByIdIntegerValue(elementId, value){
+    document.getElementById(elementId).textContent = value;
+}
